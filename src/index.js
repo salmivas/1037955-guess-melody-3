e79924641c1dd3eb367529beacde4b3ecf8629c8
Ivan Salmin <salmin.iv@gmail.com>
2020-02-05 17:41:59 +0400

    Appends React to the project. Creates first React-component.

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./components/app/app.jsx";
+
+const Settings = {
+  ERRORS_COUNT: 3
+};
+
+ReactDom.render(
+    <App
+      errorsCount = {Settings.ERRORS_COUNT}
+    />,
+    document.querySelector(`#root`)
+);
